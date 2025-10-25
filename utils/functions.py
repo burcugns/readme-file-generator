2edce4cb0d0cfc1cff4a371e48c24801b522e17c
Burcu Gunes <burcudagli.10@gmail.com>
2025-10-25 20:21:16 +0100

    create readme file function added

--- utils/functions.py
+++ utils/functions.py
@@ -1,3 +1,37 @@
+#create README file function
+ 
+def createReadmeFile(prompt):
+    return f"""
+  
+# {prompt.title.capitalize()}
+
+
+{prompt.description.capitalize()}
+
+
+## Installation
+ ```bash
+{prompt.installation}
+```
+
+## Usage
+```python
+{prompt.usage}
+```
+
+## License
+{prompt.license}
+
+
+## Author
+{prompt.author.capitalize()}
+
+## Contact
+{prompt.contact}
+
+  """
+
+# get user input function
 def getUserInput(prompt_message):
     while True:
       prompt = input(prompt_message)
@@ -5,7 +39,8 @@ def getUserInput(prompt_message):
           return prompt
       else:
           print("Input cannot be empty. Please try again.")
-   
+
+# check empty input function
 def checkEmptyInput(prompt):
       if(prompt.strip() != ''):
          return True
