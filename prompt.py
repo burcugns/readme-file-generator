fdeb1ca29bff91dc46740a173bb28f1d41e0d771
Burcu Gunes <burcudagli.10@gmail.com>
2025-10-25 15:48:47 +0100

    created prompt class

---
+++ prompt.py
@@ -0,0 +1,9 @@
+class Prompt:
+    def __init__(self, title, description, installation, usage, author, contact, license):
+        self.title = title
+        self.description = description
+        self.installation = installation
+        self.usage = usage
+        self.author = author
+        self.contact = contact
+        self.license = license
